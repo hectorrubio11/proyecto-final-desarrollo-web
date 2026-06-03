1964acf6d5b2abee8bf8f4e16be8d5b9db29a93f
Hector11 <hectorrubaya76@gmail.com>
2026-06-02 22:51:32 -0700

    archivo .sql

--- database.sql
+++ database.sql
@@ -1,4 +1,23 @@
+CREATE DATABASE tienda_mvc CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; USE tienda_mvc;
 
+CREATE TABLE usuarios ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL UNIQUE, 
+    password VARCHAR(255) NOT NULL, 
+    nombre_completo VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE productos ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sku VARCHAR(50) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL,
+    precio_compra DECIMAL(10,2) NOT NULL,
+    precio_venta DECIMAL(10,2) NOT NULL,
+    existencia INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+);
 
 CREATE TABLE bitacora (
     id INT AUTO_INCREMENT PRIMARY KEY,
